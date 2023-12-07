0b9e9c6947de690b8e3172d8d4851168189500ba
ThunderWolf42 <darrelchristhoper@gmail.com>
2023-12-07 22:28:26 +0700

    first commit

---
+++ coffe_shop_app/lib/page/home_page.dart
@@ -0,0 +1,226 @@
+import 'package:coffe_shop_app/data/item_coffee_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:coffe_shop_app/data/coffee.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: SizedBox(
+                width: 161,
+                height: 37,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Location",
+                      style: TextStyle(
+                          color: Color(0xFFB6B6B6),
+                          fontSize: 12,
+                          fontFamily: 'Sora',
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text("Menteng,Jakarta",
+                        style: TextStyle(
+                            color: Color(0xFFDDDDDD),
+                            fontSize: 14,
+                            fontFamily: 'Sora',
+                            fontWeight: FontWeight.w600,
+                            height: 0))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30),
+            child: Container(
+              width: 44,
+              height: 44,
+              decoration: ShapeDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('images/Image.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14))),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 0, 0, 0),
+            Color.fromARGB(255, 201, 119, 57)
+          ],
+          stops: [0.4, 0.52],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity, // Use double.infinity for full width
+              height: 200,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(-0.87, 0),
+                  end: Alignment(0.87, 0.55),
+                  colors: [Color(0xFF131313), Color(0xFF131313)],
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 125),
+                    child: Container(
+                      height: 52,
+                      width: 330,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6.0,
+                        horizontal: 10.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(12.0),
+                        ),
+                        border: Border.all(
+                          width: 1.0,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.search),
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              initialValue: null,
+                              decoration: const InputDecoration.collapsed(
+                                filled: true,
+                                fillColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                hintText: "Search Coffee",
+                              ),
+                              onFieldSubmitted: (value) {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset('images/coffee_promo.png'),
+            ),
+            const DefaultTabController(
+                length: 6,
+                child: TabBar(isScrollable: true, tabs: [
+                  Tab(
+                    child: Text(
+                      "Cappucino ",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  Tab(
+                    child: Text("Machiato"),
+                  ),
+                  Tab(
+                    child: Text("Latte"),
+                  ),
+                  Tab(
+                    child: Text("Arabica"),
+                  ),
+                  Tab(
+                    child: Text("Expreso"),
+                  ),
+                  Tab(
+                    child: Text("Americano"),
+                  ),
+                ])),
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.all(20),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.7,
+                ),
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return item_Coffee(coffee: listCoffee[index]);
+                },
+                itemCount: listCoffee.length,
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: ShapeDecoration(
+            color: Colors.black,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: GNav(
+              backgroundColor: Colors.black,
+              color: Colors.yellow,
+              activeColor: Colors.yellow,
+              tabBackgroundColor: Color.fromARGB(255, 15, 99, 117),
+              gap: 5,
+              padding: EdgeInsets.all(8),
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(icon: Icons.favorite_border, text: 'Favorites'),
+                GButton(
+                  icon: Icons.shopping_basket_outlined,
+                  text: 'Shop',
+                ),
+                GButton(
+                  icon: Icons.notifications,
+                  text: 'Notification',
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
